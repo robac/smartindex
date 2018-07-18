b33b10569a5eb3ca17177f7fa906f1326241b3dd
robac <robert@poch.cz>
2018-07-18 13:45:50 +0200

    test

--- exe/ajax.php
+++ exe/ajax.php
@@ -78,7 +78,7 @@ class AjaxRequestHandler {
                 $this->handle_page_info();
                 break;
             
-            default: echo "nerozumidm";
+            default: echo "nerozumidm ".$action;
         }
         
     }
