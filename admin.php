b764af421b5e9a12cbe726652c0232b685239aa7
robac <robert@poch.cz>
2018-07-25 13:34:47 +0200

    removed old admin page

--- admin.php
+++ admin.php
@@ -11,8 +11,29 @@ class admin_plugin_smartindex extends DokuWiki_Admin_Plugin {
   }
  
   function html() {
-    echo file_get_contents(HTML_DIR.'admin.html');
-    echo HtmlHelper::createInlineScript('var collectorURL = "'.AJAX_URL.'";');
+    $template = new Monotek\MiniTPL\Template(TEMPLATES_DIR);
+    $template->load("test.php");
+    $template->render();
+
+    //echo HtmlHelper::createInlineScript('var collectorURL = "'.AJAX_URL.'";');
+      /*      $config = new SmartIndexConf();
+      $config->namespace = $_POST['namespace'];
+      $config->openDepth = $_POST['depth'];
+      $config->theme = $_POST['theme'];
+      $config->checkHandle();
+      $config->checkRender();
+      if (!is_null($config->error)) {
+          $res .= "<div class=\"smartindex-error\">SmartIndex error: {$config->error}</div>";
+          echo $res;
+      } else {
+          $seeker = new PageSeeker($config);
+          $data = $seeker->get($config);
+
+          $indexBuilder = $config->getRenderer();
+          $indexBuilder->setWrapper(false);
+          $indexBuilder->render($data, $res);
+          echo $res;
+      }*/
   }
   
   function getMenuText($language) {
