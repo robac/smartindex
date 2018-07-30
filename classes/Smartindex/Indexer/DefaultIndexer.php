7ba13c3c52535a6c8e94f4f9c247ef6f38f7ed42
robac <robert@poch.cz>
2018-07-30 16:14:43 +0200

    refactor constants

--- classes/Smartindex/Indexer/DefaultIndexer.php
+++ classes/Smartindex/Indexer/DefaultIndexer.php
@@ -20,7 +20,7 @@ class DefaultIndexer implements \Smartindex\Indexer\iIndexer
     private function checkFollowPath($namespace, $level)
     {
         if ($this->info[$level - 1][iIndexer::INFO_FOLLOW] && isset($this->follow[$level]) && ($this->follow[$level] == $namespace)) {
-            $this->info[$level][self::INFO_FOLLOW] = true;
+            $this->info[$level][iIndexer::INFO_FOLLOW] = true;
             return true;
         } else {
             return false;
@@ -30,9 +30,9 @@ class DefaultIndexer implements \Smartindex\Indexer\iIndexer
     private function init()
     {
         $this->info = array();
-        $this->info[1][self::INFO_NS] = $this->config->namespace;
-        $this->info[1][self::INFO_DIR] = PageTools::getPageDirFromNamespace($this->config->baseDir, $this->config->namespace);
-        $this->info[1][self::INFO_FOLLOW] = true;
+        $this->info[1][iIndexer::INFO_NS] = $this->config->namespace;
+        $this->info[1][iIndexer::INFO_DIR] = PageTools::getPageDirFromNamespace($this->config->baseDir, $this->config->namespace);
+        $this->info[1][iIndexer::INFO_FOLLOW] = true;
 
         $this->follow = explode(PageTools::$NS_SEPARATOR, $this->config->followPath);
         unset($this->follow[count($this->follow) - 1]);
@@ -41,8 +41,8 @@ class DefaultIndexer implements \Smartindex\Indexer\iIndexer
 
     private function addInfo($level, $dir)
     {
-        $this->info[$level][self::INFO_NS] = PageTools::constructPageName($this->info[$level - 1][self::INFO_NS], $dir);
-        $this->info[$level][self::INFO_DIR] = $this->info[$level - 1][self::INFO_DIR] . '/' . $NS_SEPARATOR . $dir;
+        $this->info[$level][iIndexer::INFO_NS] = PageTools::constructPageName($this->info[$level - 1][iIndexer::INFO_NS], $dir);
+        $this->info[$level][iIndexer::INFO_DIR] = $this->info[$level - 1][iIndexer::INFO_DIR] . '/' . $NS_SEPARATOR . $dir;
     }
 
 
@@ -56,36 +56,36 @@ class DefaultIndexer implements \Smartindex\Indexer\iIndexer
 
     private function search(&$data, $level)
     {
-        $namespace = $this->info[$level][self::INFO_NS];
+        $namespace = $this->info[$level][iIndexer::INFO_NS];
 
-        $data[$namespace][self::KEY_DIRS] = array();
-        $data[$namespace][self::KEY_PAGES] = array();
-        $data[$namespace][self::KEY_PAGES_TITLE] = array();
+        $data[$namespace][iIndexer::KEY_DIRS] = array();
+        $data[$namespace][iIndexer::KEY_PAGES] = array();
+        $data[$namespace][iIndexer::KEY_PAGES_TITLE] = array();
 
-        $dh = @opendir($this->info[$level][self::INFO_DIR]);
+        $dh = @opendir($this->info[$level][iIndexer::INFO_DIR]);
         if (!$dh) return;
 
         while (($file = readdir($dh)) !== false) {
             if (preg_match('/^[\._]/', $file)) continue;
-            $filePath = $this->info[$level][self::INFO_DIR] . '/' . $file;
+            $filePath = $this->info[$level][iIndexer::INFO_DIR] . '/' . $file;
             if (is_dir($filePath)) {
-                $data[$namespace][self::KEY_DIRS][] = $file;
+                $data[$namespace][iIndexer::KEY_DIRS][] = $file;
                 continue;
             }
             $pagename = PageTools::excludePageExtension($file);
-            $data[$namespace][self::KEY_PAGES][] = $pagename;
-            $title = p_get_first_heading(PageTools::constructPageName($this->info[$level][self::INFO_NS], $pagename));
-            $data[$namespace][self::KEY_PAGES_TITLE][] = ($title != null) ? $title : $pagename;
+            $data[$namespace][iIndexer::KEY_PAGES][] = $pagename;
+            $title = p_get_first_heading(PageTools::constructPageName($this->info[$level][iIndexer::INFO_NS], $pagename));
+            $data[$namespace][iIndexer::KEY_PAGES_TITLE][] = ($title != null) ? $title : $pagename;
         }
         closedir($dh);
-        /*        array_multisort(array_map('strtolower', $data[$namespace][self::KEY_PAGES_TITLE]), SORT_STRING,
-                                $data[$namespace][self::KEY_PAGES], SORT_STRING);*/
+        /*        array_multisort(array_map('strtolower', $data[$namespace][iIndexer::KEY_PAGES_TITLE]), SORT_STRING,
+                                $data[$namespace][iIndexer::KEY_PAGES], SORT_STRING);*/
 
-        if (($level < $this->config->openDepth) || $this->info[$level][self::INFO_FOLLOW]) {
-            foreach ($data[$namespace][self::KEY_DIRS] as $subdir) {
+        if (($level < $this->config->openDepth) || $this->info[$level][iIndexer::INFO_FOLLOW]) {
+            foreach ($data[$namespace][iIndexer::KEY_DIRS] as $subdir) {
                 $isFollow = $this->checkFollowPath($subdir, $level + 1);
                 if ($isFollow) {
-//                    $data[$namespace][self::KEY_FOLLOW] = true;
+//                    $data[$namespace][iIndexer::KEY_FOLLOW] = true;
                 }
                 if (($level < $this->config->openDepth) || $isFollow) {
                     $this->addInfo($level + 1, $subdir);
