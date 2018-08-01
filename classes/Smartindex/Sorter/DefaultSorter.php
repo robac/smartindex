a5f31d0fc3025a90d3b4e6c328289a116c57c6c8
robac <robert@poch.cz>
2018-08-01 13:02:01 +0200

    Default sorter bug fixed

--- classes/Smartindex/Sorter/DefaultSorter.php
+++ classes/Smartindex/Sorter/DefaultSorter.php
@@ -14,9 +14,9 @@ class DefaultSorter implements \Smartindex\Sorter\iPageSorter
     public function sort($data)
     {
         $isnamespace = array();
-        $res[0] = array_merge($data[$this->config->namespace][0], $data[$this->config->namespace][1]);
-        $res[1] = array_merge(array_fill(0, count($data[$this->config->namespace][0]), true), array_fill(0, count($data[$this->config->namespace][1]), false));
-        $res[2] = array_merge($data[$this->config->namespace][0], $data[$this->config->namespace][1]);
+        $res[0] = array_merge($data[$this->config->namespace]["namespaces"], $data[$this->config->namespace][1]);
+        $res[1] = array_merge(array_fill(0, count($data[$this->config->namespace]["namespaces"]), true), array_fill(0, count($data[$this->config->namespace][1]), false));
+        $res[2] = array_merge($data[$this->config->namespace]["namespaces"], $data[$this->config->namespace][1]);
         return $res;
     }
 }
