58cd63d14941318d99388dbacfc361096bd9167f
robac <robert@poch.cz>
2018-08-02 13:18:36 +0200

    DefaultRenderer refactor

--- classes/Smartindex/Renderer/DefaultRenderer.php
+++ classes/Smartindex/Renderer/DefaultRenderer.php
@@ -1,7 +1,12 @@
 <?php
 namespace Smartindex\Renderer;
 
-class DefaultRenderer implements \Smartindex\Renderer\iIndexRenderer {
+use Smartindex\Utils\HtmlHelper;
+use Smartindex\Renderer\iIndexRenderer;
+use Smartindex\Configuration\IndexConfiguration;
+use Smartindex\Configuration\IndexTools;
+
+class DefaultRenderer implements IndexRenderer {
     private $useWrapper = true;
     private $wrapperClasses = array();
     private $wrapperId;
@@ -13,26 +18,25 @@ class DefaultRenderer implements \Smartindex\Renderer\iIndexRenderer {
     public function setWrapper($useWrapper, $id = NULL) {
         $this->useWrapper = $useWrapper;
         
-        $this->wrapperClasses[] = \Smartindex\Configuration\IndexConfiguration::TREE_CLASS;
+        $this->wrapperClasses[] = IndexConfiguration::TREE_CLASS;
         $this->wrapperClasses[] = $this->config->getAttribute('cssClass');
         if ($this->config->getAttribute('highlight')) {
-            $this->wrapperClasses[] = \Smartindex\Configuration\IndexConfiguration::HIGHLITE_CLASS;
+            $this->wrapperClasses[] = IndexConfiguration::HIGHLITE_CLASS;
         }
         
         $this->wrapperId = $id;
     }
 
-    public function __construct(\Smartindex\Configuration\IndexConfiguration $config) {
+    public function __construct(IndexConfiguration $config) {
         $this->config = $config;
     }
     
     public function render($data, &$document) {
         if ($this->useWrapper) {
-            $document .= "<div". \Smartindex\Utils\HtmlHelper::createIdClassesPart($this->wrapperId, $this->wrapperClasses).">";
+            $document .= "<div". HtmlHelper::createIdClassesPart($this->wrapperId, $this->wrapperClasses).">";
         }
         
         $this->basicData = $data;
-        //if ($this->config->showMain) $document .= "<ul><li class=\"namespace open\"><div><a href=\"#\">root</a></div>";
         $this->buildList($data, $this->config->getAttribute('namespace'), $document, 1);
 
         if ($this->useWrapper) {
@@ -45,7 +49,6 @@ class DefaultRenderer implements \Smartindex\Renderer\iIndexRenderer {
         $ajaxConfig->theme = $this->config->getAttribute('theme');
 
         $document->doc .= HtmlHelper::createInlineScript(HtmlHelper::createInlineJSON($this->config->getAttribute('treeId')."_conf", $ajaxConfig));
-
     }
     
     private function buildList($data, $namespace, &$document, $level) {
@@ -54,31 +57,31 @@ class DefaultRenderer implements \Smartindex\Renderer\iIndexRenderer {
         
         $document .= "<ul>";
         
-        foreach($data[$namespace][\Smartindex\Indexer\iIndexer::KEY_DIRS] as $ns)  {
+        foreach($data[$namespace][iIndexer::KEY_DIRS] as $ns)  {
             $classes = array(self::CLASS_NAMESPACE);
             
-            if (($this->config->getAttribute('openDepth') > $level) || (isset($data[$ns][\Smartindex\Indexer\iIndexer::KEY_FOLLOW]))) {
+            if (($this->config->getAttribute('openDepth') > $level) || (isset($data[$ns][iIndexer::KEY_FOLLOW]))) {
                 $classes[] = self::CLASS_OPEN;
             } else {
                 $classes[] = self::CLASS_CLOSED;
             }
 
                 
-            $document .= "<li". \Smartindex\Utils\HtmlHelper::createIdClassesPart(NULL, $classes)."><div>"
-                         . \Smartindex\Utils\HtmlHelper::createSitemapLink(\Smartindex\Utils\IndexTools::constructPageName($namespace, $ns), $ns)
+            $document .= "<li". HtmlHelper::createIdClassesPart(NULL, $classes)."><div>"
+                         . HtmlHelper::createSitemapLink(IndexTools::constructPageName($namespace, $ns), $ns)
                          ."</div>";
             
-            $this->buildList($data, \Smartindex\Utils\IndexTools::constructPageName($namespace, $ns), $document, $level+1);
+            $this->buildList($data, IndexTools::constructPageName($namespace, $ns), $document, $level+1);
             $document .= "</li>";
         }
         
         
-        foreach($data[$namespace][\Smartindex\Indexer\iIndexer::KEY_PAGES] as $key => $page) {
-            $heading = $data[$namespace][\Smartindex\Indexer\iIndexer::KEY_PAGES_TITLE][$key];
+        foreach($data[$namespace][iIndexer::KEY_PAGES] as $key => $page) {
+            $heading = $data[$namespace][iIndexer::KEY_PAGES_TITLE][$key];
             if ($heading == "")
                 $heading = $page;
-            $document .= "<li". \Smartindex\Utils\HtmlHelper::createIdClassesPart(NULL, array(self::CLASS_PAGE))."><div>"
-                         . \Smartindex\Utils\HtmlHelper::createInternalLink(\Smartindex\Utils\IndexTools::constructPageName($namespace, $page), NULL, $heading, NULL, NULL)
+            $document .= "<li". HtmlHelper::createIdClassesPart(NULL, array(self::CLASS_PAGE))."><div>"
+                         . HtmlHelper::createInternalLink(IndexTools::constructPageName($namespace, $page), NULL, $heading, NULL, NULL)
                          ."</div ></li>";
         }
         
