2f86950dfb68c25e6ab0eeba3a1e2c78267394bf
robac <robert@poch.cz>
2018-08-16 10:51:18 +0200

    twig added

--- classes/Twig/Extension/InitRuntimeInterface.php
+++ classes/Twig/Extension/InitRuntimeInterface.php
@@ -1,11 +1,32 @@
 <?php
 
-namespace Twig\Extension;
+/*
+ * This file is part of Twig.
+ *
+ * (c) Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-class_exists('Twig_Extension_InitRuntimeInterface');
-
-if (\false) {
-    interface InitRuntimeInterface extends \Twig_Extension_InitRuntimeInterface
-    {
-    }
+/**
+ * Enables usage of the deprecated Twig_Extension::initRuntime() method.
+ *
+ * Explicitly implement this interface if you really need to implement the
+ * deprecated initRuntime() method in your extensions.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+interface Twig_Extension_InitRuntimeInterface
+{
+    /**
+     * Initializes the runtime environment.
+     *
+     * This is where you can load some file that contains filter functions for instance.
+     *
+     * @param Twig_Environment $environment The current Twig_Environment instance
+     */
+    public function initRuntime(Twig_Environment $environment);
 }
+
+class_alias('Twig_Extension_InitRuntimeInterface', 'Twig\Extension\InitRuntimeInterface', false);
